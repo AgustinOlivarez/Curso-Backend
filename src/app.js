38ccb80151d6183d7a0683ce0cc27bede9423171
Agustin Olivarez <agustinolivarez29@gmail.com>
2023-09-29 20:18:58 -0300

    Instalando socket y handlebars

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 import express from "express";
+import { engine } from "express-handlebars";
 import productsRouter from "./router/productsRoute.js";
 import cartRouter from "./router/cartRoute.js";
+import { __dirname } from "./utils.js";
+import { Server } from "socket.io";
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+// handlebars
+app.engine("handlebars", engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+
+const PORT = 8080;
+
+const httpServer = app.listen(PORT, () => {
+  console.log(`Escuchando al puerto ${PORT}`);
+});
+
+// websocket - server
+const socketServer = new Server(httpServer);
 
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
 
-app.listen(8080, () => {
-  console.log("Escuchando al puerto 8080");
-});
+// app.listen(8080, () => {
+//   console.log("Escuchando al puerto 8080");
+// });
--- src/router/cartRoute.js
+++ src/router/cartRoute.js
@@ -1,6 +1,6 @@
 import Router from "express";
 
-import CartManager from "../CartManager.js";
+import CartManager from "../managers/CartManager.js";
 
 const router = Router();
 
